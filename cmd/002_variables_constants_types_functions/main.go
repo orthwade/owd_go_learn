d83119642142d0d572887844514aeec84b852a1f
orthwade <orthwade@protonmail.com>
2025-04-21 01:33:38 +0500

    002. tried variadic function, passing func as paramter and lambda

--- cmd/002_variables_constants_types_functions/main.go
+++ cmd/002_variables_constants_types_functions/main.go
@@ -14,9 +14,38 @@ var message string = fmt.Sprintf(
 	name, ageFloat, height,
 )
 
+func sum(args ...int) int {
+	// Sum all the integers in args
+	total := 0
+
+	for _, arg := range args {
+		total += arg
+	}
+
+	return total
+}
+
+func add(v_0 int, v_1 int) int {
+	return v_0 + v_1
+}
+
+func applyFunc(f func(int, int) int, val_1 int, val_2 int) int {
+	return f(val_1, val_2)
+}
+
 func main() {
 	// Print the message
 	fmt.Println(message)
+
+	fmt.Println("Sum of 1, 2, 3, 4, 5:", sum(1, 2, 3, 4, 5))
+
+	fmt.Println("Sum of 10, 20:", applyFunc(add, 10, 20))
+
+	lambdaAdd := func(v_0 int, v_1 int) int {
+		return v_0 + v_1
+	}
+
+	fmt.Println("Lambda sum of 10, 20:", lambdaAdd(10, 20))
 
 	// Basic function (placeholder for additional code)
 }
