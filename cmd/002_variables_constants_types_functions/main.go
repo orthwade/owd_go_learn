0b6dcbf815d408fc9e73a1c5370e49da5d8a1f26
orthwade <orthwade@protonmail.com>
2025-04-21 01:14:28 +0500

    002. trying functions

--- cmd/002_variables_constants_types_functions/main.go
+++ cmd/002_variables_constants_types_functions/main.go
@@ -2,16 +2,21 @@ package main
 
 import "fmt"
 
+// Variables
 var name string = "Orthwade"
-
 var age int = 30
-
-var ageFltoat float32 = float32(age)
-
+var ageFloat float32 = float32(age)
 var height float32 = 1.80
 
-var message string = fmt.Sprintf("My name is %s, I am %f years old and my height is %.2f meters.", name, ageFltoat, height)
+// Formatted message
+var message string = fmt.Sprintf(
+	"My name is %s, I am %.2f years old and my height is %.2f meters.",
+	name, ageFloat, height,
+)
 
 func main() {
+	// Print the message
 	fmt.Println(message)
+
+	// Basic function (placeholder for additional code)
 }
