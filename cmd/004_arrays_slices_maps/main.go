e015ab24d65c3b288e498d70f8f296248058d9af
orthwade <orthwade@protonmail.com>
2025-04-22 17:42:10 +0500

    004. tried arrays and slices

--- cmd/004_arrays_slices_maps/main.go
+++ cmd/004_arrays_slices_maps/main.go
@@ -1,4 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
+	arrInt := [3]int{1, 2, 3}
+
+	sliceInt := []int{1, 2, 3}
+
+	fmt.Println("Array:", arrInt)
+	fmt.Println("Slice:", sliceInt)
+
+	fmt.Println("Adding 4 to slice")
+	sliceInt = append(sliceInt, 4)
+	fmt.Println("Slice:", sliceInt)
+
 }
